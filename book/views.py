200b152f47f438958f96ae756a7f47c349c83c13
Alimsaid <alimsaid242@gmail.com>
2022-02-16 19:38:49 +0600

    alim

--- book/views.py
+++ book/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from . import models
+from . import models, forms
 from django.shortcuts import get_object_or_404
+from django.shortcuts import reverse, redirect
+# from django.http import HttpResponse
 
 
 def get_books_all(request):
@@ -11,4 +13,17 @@ def get_books_all(request):
 def get_books_detail(request, id):
     book = get_object_or_404(models.Books, id=id)
     comment_id = models.BookComments.objects.filter(books_id=id)
-    return render(request, "books_detail.html", {"book": book,'comment':comment_id})
+    return render(request, "books_detail.html", {"book": book, 'comment': comment_id})
+
+
+def all_books(request):
+    method = request.method
+    if method == "POST":
+        form = forms.Books_all(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse("books:books_list"))
+            # return HttpResponse("Books Created Successfully")
+    else:
+        form = forms.Books_all()
+    return render(request, "add_books.html", {"form": form})
