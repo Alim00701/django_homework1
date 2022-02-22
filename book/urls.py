9fee63a42ab82c2d62ee6d16341f8f3b1b940725
Alimsaid <alimsaid242@gmail.com>
2022-02-23 00:57:48 +0600

    alim

--- book/urls.py
+++ book/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from . import views
+from . import views, models
 
 app_name = "books"
 urlpatterns = [
-    path('books/', views.get_books_all, name="books_list"),
-    path('books/<int:id>/', views.get_books_detail, name="books_detail"),
-    path('books/<int:id>/update/', views.put_books_update, name="books_update"),
-    path('books/<int:id>/delete/', views.book_delete, name="books_delete"),
-    path('add-books/', views.all_books, name="add_books")
+    path('books/', views.BooksListView.as_view(), name="books_list"),
+    path('books/comedy/', views.BooksListView.as_view(queryset=models.Books.objects.filter(genre="Comedy").order_by("-created_date")), name="books_comedy_list"),
+    path('books/horror/', views.BooksListView.as_view(queryset=models.Books.objects.filter(genre="Horror").order_by("-created_date")), name="books_horror_list"),
+    path('books/drama/', views.BooksListView.as_view(queryset=models.Books.objects.filter(genre="Drama").order_by("-created_date")), name="books    _drama_list"),
+    path('books/<int:id>/', views.BooksDetailView.as_view(), name="books_detail"),
+    path('books/<int:id>/update/', views.BooksUpdateView.as_view(), name="books_update"),
+    path('books/<int:id>/delete/', views.BooksDeleteView.as_view(), name="books_delete"),
+    path('add-books/', views.BooksCreateView.as_view(), name="add_books")
 ]
--- book/views.py
+++ book/views.py
@@ -3,48 +3,98 @@ from . import models, forms
 from django.shortcuts import get_object_or_404
 from django.shortcuts import reverse, redirect
 from django.http import HttpResponse
+from django.views import generic
 
 
-def get_books_all(request):
-    books = models.Books.objects.all()
-    return render(request, "books_list.html", {"books": books})
+class BooksListView(generic.ListView):
+    template_name = "books_list.html"
+    queryset = models.Books.objects.all()
+
+    def get_queryset(self):
+        return self.queryset
 
 
-def get_books_detail(request, id):
-    book = get_object_or_404(models.Books, id=id)
-    comment_id = models.BookComments.objects.filter(books_id=id)
-    return render(request, "books_detail.html", {"book": book, 'comment': comment_id})
+# def get_books_all(request):
+#     books = models.Books.objects.all()
+#     return render(request, "books_list.html", {"books": books})
 
 
-def all_books(request):
-    method = request.method
-    if method == "POST":
-        form = forms.Books_all(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect(reverse("books:books_list"))
-            # return HttpResponse("Books Created Successfully")
-    else:
-        form = forms.Books_all()
-    return render(request, "add_books.html", {"form": form})
+class BooksDetailView(generic.DetailView):
+    template_name = "books_detail.html"
+
+    def get_object(self, **kwargs):
+        books_id = self.kwargs.get("id")
+        return get_object_or_404(models.Books, id=books_id)
 
 
-def put_books_update(request, id):
-    book_id = get_object_or_404(models.Books, id=id)
-    if request.method == "POST":
-        form = forms.Books_all(instance=book_id,
-                               data=request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect(reverse("books:books_list"))
-    else:
-        form = forms.Books_all(instance=book_id)
-    return render(request, "books_update.html", {"form": form,
-                                                 "book": book_id})
+# def get_books_detail(request, id
+#     book = get_object_or_404(models.Books, id=id)
+#     comment_id = models.BookComments.objects.filter(books_id=id)
+#     return render(request, "books_detail.html", {"book": book, 'comment': comment_id})):
 
 
-def book_delete(request, id):
-    book_id = get_object_or_404(models.Books, id=id)
-    book_id.delete()
-    # return HttpResponse("Book Deleted")
-    return redirect(reverse("books:books_list"))
+class BooksCreateView(generic.CreateView):
+    template_name = "add_books.html"
+    form_class = forms.Books_all
+    queryset = models.Books.objects.all()
+    success_url = "/books/"
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super(BooksCreateView, self).form_valid(form=form)
+
+
+# def all_books(request):
+#     method = request.method
+#     if method == "POST":
+#         form = forms.Books_all(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return redirect(reverse("books:books_list"))
+#             # return HttpResponse("Books Created Successfully")
+#     else:
+#         form = forms.Books_all()
+#     return render(request, "add_books.html", {"form": form})
+
+
+class BooksUpdateView(generic.UpdateView):
+    template_name = "books_update.html"
+    form_class = forms.Books_all
+    success_url = "/books/"
+
+    def get_object(self, **kwargs):
+        books_id = self.kwargs.get("id")
+        return get_object_or_404(models.Books, id=books_id)
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super(BooksUpdateView, self).form_valid(form=form)
+
+
+# def put_books_update(request, id):
+#     book_id = get_object_or_404(models.Books, id=id)
+#     if request.method == "POST":
+#         form = forms.Books_all(instance=book_id,
+#                                data=request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect(reverse("books:books_list"))
+#     else:
+#         form = forms.Books_all(instance=book_id)
+#     return render(request, "books_update.html", {"form": form,
+#                                                  "book": book_id})
+
+class BooksDeleteView(generic.DeleteView):
+    template_name = "confirm_delete_book.html"
+    success_url = "/books/"
+
+    def get_object(self, **kwargs):
+        books_id = self.kwargs.get("id")
+        return get_object_or_404(models.Books, id=books_id)
+
+
+# def book_delete(request, id):
+#     book_id = get_object_or_404(models.Books, id=id)
+#     book_id.delete()
+#     # return HttpResponse("Book Deleted")
+#     return redirect(reverse("books:books_list"))
