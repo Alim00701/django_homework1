9a1b2bca2fcc27084c9732279b62842dff4873db
Alimsaid <alimsaid242@gmail.com>
2022-02-27 16:48:30 +0600

    alim

--- main/settings.py
+++ main/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'book',
+    'parser_app',
 ]
 
 MIDDLEWARE = [
---
+++ parser_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'parser'
+urlpatterns = [
+    path('parser/', views.ParserFormView.as_view(), name="parser"),
+    path('film/', views.FilmListView.as_view(), name="film_list"),
+    path('film/<int:id>/', views.FilmsDetailView.as_view(), name="film_detail")
+]
